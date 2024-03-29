ad251c47f416108c80b9b0f1ab91216bc57175cf
MykytaH <nikita.gavrilenko.1141@gmail.com>
2024-03-30 01:28:30 +0300

    gh-pages

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,25 @@ import Store from "./pages/Store";
 import Cart from "./pages/Cart";
 import RootLayout from "./pages/RootLayout";
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <RootLayout />,
+      // errorElement: <Error />,
+      children: [
+        { index: true, element: <Store /> },
+        {
+          path: "cart",
+          element: <Cart />,
+        },
+      ],
+    },
+  ],
   {
-    path: "/testNeoflex",
-    element: <RootLayout />,
-    // errorElement: <Error />,
-    children: [
-      { index: true, element: <Store /> },
-      {
-        path: "cart",
-        element: <Cart />,
-      },
-    ],
-  },
-]);
+    basename: "/testNeoflex",
+  }
+);
 
 export default function App() {
   return <RouterProvider router={router} />;
